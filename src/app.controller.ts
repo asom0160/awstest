bc03830d3d63e2f386061e762caba547a720a16b
yoo gayeong <asom8650@gmail.com>
2025-05-07 11:24:31 +0900

    1

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get("test")
+  getTest(): Object {
+    return { data: '테스트임둥', message: '테스트' }
+  }
+  @Get("test")
+  getSiuuu(): Object {
+    return { data: 'Siuuu', message: 'Siuuu' }
+  }
+}
